7e8b55b5e217c8dd9f6999556409ef83f4e51e8b
Kwabena N Amponsah <kwabenantim@yahoo.com>
2024-04-16 18:03:25 +0000

    #29 update xsd build scripts

---
+++ infra/conda/xsd/build-package.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+set -xeo pipefail
+
+
+'linux_64_python3.10.____cpython'
+export UPLOAD_PACKAGES='False'
+export BUILD_WITH_CONDA_DEBUG='0'
+export DOCKER_IMAGE='quay.io/condaforge/linux-anvil-cos7-x86_64'
+export CPU_COUNT=4
+
+usage() {
+  echo 'Usage: '"$(basename "$0")"' --variant=name [--parallel=num]'
+  echo 'Example:'
+  echo 'docker run --rm -it quay.io/condaforge/linux-anvil-cos7-x86_64 /bin/bash'
+  echo "$(basename "$0")"' --variant=linux_64_python3.10.____cpython --parallel=4'
+  exit 1
+}
+
+# Parse args
+variant=
+parallel=
+
+for option; do
+  case $option in
+  --variant=*)
+    variant=$(expr "x$option" : "x--version=\(.*\)")
+    ;;
+  --parallel=*)
+    parallel=$(expr "x$option" : "x--parallel=\(.*\)")
+    ;;
+  *)
+    echo "Unknown option: $option" 1>&2
+    exit 1
+    ;;
+  esac
+done
+
+if [ -z "${variant}" ]; then usage; fi
+
+# Configure environment
+export FEEDSTOCK_ROOT="$(pwd)"
+export RECIPE_ROOT="${FEEDSTOCK_ROOT}/recipe"
+export CONFIG_FILE="${FEEDSTOCK_ROOT}/variants/${variant}.yaml"
+export CONDA_BLD_PATH="${FEEDSTOCK_ROOT}/build_artifacts"
+
+export CPU_COUNT="${parallel:-$(nproc)}"
+
+export PYTHONUNBUFFERED=1
+
+# Configure conda build path
+mkdir -p "${CONDA_BLD_PATH}"
+
+cat >~/.condarc <<CONDARC
+conda-build:
+  root-dir: ${CONDA_BLD_PATH}
+pkgs_dirs:
+  - ${CONDA_BLD_PATH}/pkg_cache
+  - /opt/conda/pkgs
+CONDARC
+
+# Install conda build tools
+mamba install --update-specs --yes --quiet --channel conda-forge \
+  conda-build pip boa liblief=0.11.5 conda-forge-ci-setup=3
+mamba update --update-specs --yes --quiet --channel conda-forge \
+  conda-build pip boa liblief=0.11.5 conda-forge-ci-setup=3
+
+# Configure conda channels
+conda config --add channels conda-forge
+conda config --add channels pychaste
+conda config --env --set show_channel_urls true
+conda config --env --set auto_update_conda false
+conda config --env --set add_pip_as_python_dependency false
+conda config --env --append aggressive_update_packages ca-certificates
+conda config --env --remove-key aggressive_update_packages
+conda config --env --append aggressive_update_packages ca-certificates
+conda config --env --append aggressive_update_packages certifi
+conda config --env --set channel_priority strict
+
+# Configure conda activation
+mkdir -p "${CONDA_PREFIX}/etc/conda/activate.d"
+cat >"${CONDA_PREFIX}"/etc/conda/activate.d/conda-forge-ci-setup-activate.sh <<CONDAACTIVATE
+export CONDA_BLD_PATH='${CONDA_BLD_PATH}'
+export CPU_COUNT='${CPU_COUNT:-}'
+export PYTHONUNBUFFERED='1'
+CONDAACTIVATE
+
+# Show conda build configuration
+cat "${CONFIG_FILE}"
+
+conda info
+conda config --env --show-sources
+conda list --show-channel-urls
+
+# Build conda package
+conda mambabuild "${RECIPE_ROOT}" -m "${CONFIG_FILE}"
---
+++ infra/conda/xsd/recipe/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp "$SRC_DIR/bin/xsd" "$PREFIX/bin/xsd"
+cp -r "$SRC_DIR/libxsd/" "$PREFIX/include/"
