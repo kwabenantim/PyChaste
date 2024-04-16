e41d0bd28eadf4ecda2f0dea17f151ffbd7b0f15
Kwabena N Amponsah <kwabenantim@yahoo.com>
2024-04-16 22:18:13 +0000

    #29 relax xsd xerces dependency

--- infra/conda/xsd/4.0/recipe/build.sh
+++ infra/conda/xsd/4.0/recipe/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# https://github.com/Homebrew/homebrew-core/blob/3e856f62bfab2ff25405eaa9a40b4bad61bd4a54/Formula/x/xsd.rb
+mv xsd/version xsd/version.txt
+mv libxsd-frontend/version libxsd-frontend/version.txt
+mv libcutl/version libcutl/version.txt
+
 make \
   CC="$CC" \
   CXX="$CXX" \
@@ -7,8 +12,7 @@ make \
   RANLIB="$RANLIB" \
   CFLAGS="$CFLAGS" \
   CPPFLAGS="$CPPFLAGS" \
-  CXXFLAGS="$CXXFLAGS \
-  -std=c++14" \
+  CXXFLAGS="$CXXFLAGS -std=c++14" \
   LDFLAGS="$LDFLAGS" \
   -j $CPU_COUNT \
   verbose=1
