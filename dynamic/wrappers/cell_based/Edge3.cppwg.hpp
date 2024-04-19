36564584b07fdce640aaa4566ac16b5646910020
Kwabena N Amponsah <kwabenantim@yahoo.com>
2024-04-19 11:54:51 +0000

    #42 remove duplicate Edge wrappers

--- dynamic/wrappers/cell_based/Edge3.cppwg.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef Edge3_hpp__pyplusplus_wrapper
-#define Edge3_hpp__pyplusplus_wrapper
-
-namespace py = pybind11;
-void register_Edge3_class(py::module &m);
-#endif // Edge3_hpp__pyplusplus_wrapper
